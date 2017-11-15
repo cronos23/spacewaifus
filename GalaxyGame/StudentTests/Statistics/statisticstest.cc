5857e1c798a9095a2018cc49c574199c0c1ede06
Valtteri Setala <setala6@linux-desktop4.cc.tut.fi>
2017-11-15 20:02:36 +0200

    Statisticstest aloiteltu

--- GalaxyGame/StudentTests/Statistics/statisticstest.cc
+++ GalaxyGame/StudentTests/Statistics/statisticstest.cc
@@ -1,6 +1,9 @@
 #include <QString>
 #include <QtTest>
 #include "statistics.hh"
+#include "stateexception.hh"
+using namespace Student;
+using namespace Common;
 class StatisticsTest : public QObject
 {
     Q_OBJECT
@@ -10,6 +13,8 @@ public:
 
 private Q_SLOTS:
     void testCase1();
+    void testReducePointsBelowZero();
+
 };
 
 StatisticsTest::StatisticsTest()
@@ -21,6 +26,16 @@ void StatisticsTest::testCase1()
 {
     QVERIFY2(true, "Failure");
 }
+
+void StatisticsTest::testReducePointsBelowZero()
+{
+    Statistics stat;
+    stat.addPoints(10);
+    QVERIFY_EXCEPTION_THROWN(stat.reducePoints(20), StateException);
+}
+
+
+
 
 QTEST_APPLESS_MAIN(StatisticsTest)
 

abb6769acd097d591db5d052f1c229cc8143ea25
Ossi <ossi.kronlof@student.tut.fi>
2017-11-20 20:39:47 +0000

    lisätty starsystemeille qitem-luokka piirtämistä varten

---
+++ GalaxyGame/Student/starsystemobject.cpp
@@ -0,0 +1,3 @@
+#include "starsystemobject.hh"
+
+
---
+++ GalaxyGame/Student/starsystemobject.hh
@@ -0,0 +1,12 @@
+#ifndef STARSYSTEMOBJECT_HH
+#define STARSYSTEMOBJECT_HH
+
+#include <QGraphicsEllipseItem>
+
+class starsystemobject: public QGraphicsEllipseItem {
+
+public:
+
+};
+
+#endif // STARSYSTEMOBJECT_HH
